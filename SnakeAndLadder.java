67d393a204df6fcae2386e960e739f1fef7b4c74
Arunb2401 <arunbiradar24@gmail.com>
2021-07-27 11:59:00 +0530

    Added: Snake and Ladder game played with single player at position 0

---
+++ SnakeAndLadder.java
@@ -0,0 +1,13 @@
+package snake_and_ladder;
+
+public class SnakeAndLadder 
+{
+	
+	public static final int STARTING_POINT = 0;
+	public static void main(String[] args) 
+	{
+		System.out.println("STARTING_POINT");
+
+	}
+
+}
