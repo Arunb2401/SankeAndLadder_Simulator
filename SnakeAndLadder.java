ea30410cea752473a8deb565487ef4897d9f753d
Arunb2401 <arunbiradar24@gmail.com>
2021-07-27 13:07:15 +0530

    Added: Check for options from - Noplay,Ladder,Snake

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,27 +1,59 @@
 package snake_and_ladder;
-
+ 
 import java.util.Random; 
+public class SnakeAndLadder 
+{ 
+  
+		 static final int STARTING_POINT = 0; 
+		 static final int PLAYER=1; 
+		 static final int LADDER = 1; 
+		 static final int SNAKE = 2; 
+		 static final int NOPLAY = 0; 
+		 static Random RANDOM = new Random(); 
  
-	  public class SnakeAndLadder { 
  
- 
-      static final int STARTING_POSITION = 0; 
-	  static final int PLAYER=1; 
-	  static Random RANDOM = new Random(); 
-
- 
-       //uc2 rolling a random value to the player 
-       public void diceRoll() 
-       { 
-           int DICE = (RANDOM.nextInt(6)+1); 
-           System.out.println("dice number  " +DICE); 
+         public int DiceRoll() 
+     { 
+         int diceNumber = (RANDOM.nextInt(6)+1); 
+         System.out.println("Dice number  " +diceNumber); 
+         return diceNumber; 
        } 
-   
+     	 public void gamePlay() 
+   { 
+         int playerPosition=STARTING_POINT; 
+         int dice=DiceRoll(); 
+         int action = RANDOM.nextInt(3); 
+         System.out.println("Action: "+action); 
+         switch(action) 
+         {  
  
-       public static void main(String args[]) 
+             case LADDER: 
+                 playerPosition = playerPosition + dice; 
+                     System.out.println("Current ladder position: " + playerPosition); 
+                 break; 
+             
+             case SNAKE: 
+                 playerPosition = playerPosition - dice; 
+                 if(playerPosition<0){ 
+                     playerPosition=STARTING_POINT; 
+                 } 
+                 System.out.println("Current snake position: "+playerPosition); 
+                 break; 
+			 
+             case NOPLAY: 
+                 playerPosition = playerPosition; 
+                 if(playerPosition<0){ 
+                     playerPosition=STARTING_POINT; 
+                 } 
+                 System.out.println("No play position: "+playerPosition); 
+                 break; 
+            } 
+     } 
+         public static void main(String args[]) 
        { 
-           SnakeAndLadder snakeAndLadder=new SnakeAndLadder(); 
-          snakeAndLadder.diceRoll(); 
-      } 
-}
+         SnakeAndLadder snakeAndLadder=new SnakeAndLadder(); 
+         snakeAndLadder.gamePlay(); 
+         } 
+ 
+ } 
 
